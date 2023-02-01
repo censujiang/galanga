ba1201155b34f35f9cbcc7f33373662bbe33c614
江程训 <dfjsiou@qq.com>
2023-02-01 21:06:34 +0800

    修改文档的站点颜色布局

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,13 +3,14 @@ const now_year = new Date().getFullYear()
 export default {
   lang: 'zh-CN',
   title: '山奈Galanga',
-  description: 'Just playing around.',
+  description: '山奈Galanga是一个开源的JS函数库',
   cleanUrls: true,
   lastUpdated: true,
   themeConfig: {
     siteTitle: 'Galanga',
     nav: [
       { text: '首页', link: '/' },
+      { text: '教程', link: '/guide/' },
       {
         text: '站点切换',
         items: [
@@ -21,6 +22,9 @@ export default {
     editLink: {
       pattern: 'https://github.com/censujiang/galanga/edit/master/docs/:path',
     },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/censujiang/galanga' },
+    ],
     footer: {
       message: 'Released under Apache License 2.0',
       copyright: 'Copyright © 2014-' + now_year + ' CensuJiang All Rights Reserved.'
