2c5998c3ccffa5380c23e01457b266aa9b46f4c3
江程训 <dfjsiou@qq.com>
2023-02-02 22:59:51 +0800

    version-0.0.12

--- scripts/release.js
+++ scripts/release.js
@@ -3,8 +3,10 @@ const shell = require('shelljs');
 
 shell.exec('npm run build');
 shell.exec('npm run docs:build');
-shell.exec('git commit -am version-' + packageJson.version);
-shell.exec('git tag '+ packageJson.version);
-shell.exec('git push');
-shell.exec('git push --tags');
-shell.exec('npm publish');
+setTimeout(() => {
+  shell.exec('git commit -am version-' + packageJson.version);
+  shell.exec('git tag ' + packageJson.version);
+  shell.exec('git push');
+  shell.exec('git push --tags');
+  shell.exec('npm publish');
+}, 1000);
