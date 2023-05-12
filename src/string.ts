5a31a44924e5ac37dd9e4ce2503e88d974037410
江程训 <dfjsiou@qq.com>
2023-05-12 17:52:25 +0800

    false匹配到‘’是真垃圾真牛逼嗷

--- src/string.ts
+++ src/string.ts
@@ -1,15 +1,11 @@
 // 检查输入的值是否为空
 export function checkNull(val: any) {
-  if (val === false || val === true) {
-    return false;
+  if (val === null || val === undefined || val === '' || typeof val === 'number' && isNaN(val)) {
+    return true;
+  } else if (Array.isArray(val) && val.length === 0) {
+    return true;
   } else {
-    if (val === null || val === undefined || val === '' || typeof val === 'number' && isNaN(val)) {
-      return true;
-    } else if (Array.isArray(val) && val.length === 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return false;
   }
 }
 
