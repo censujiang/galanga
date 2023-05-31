a5ea79f1f19ade94e4c5623f9a162e7d96cd78c5
江程训 <dfjsiou@qq.com>
2023-05-31 23:15:30 +0800

    新增设置目录之类的傻卵玩意

--- src/url.ts
+++ src/url.ts
@@ -1,3 +1,5 @@
+import { checkNull } from "./string";
+
 export const url = {
 	getQuery(name: string) {
 		const result = window.location.search.match(new RegExp('[?&]' + name + '=([^&]+)', 'i'));
@@ -16,4 +18,32 @@ export const url = {
 	getPath() {
 		return window.location.pathname;
 	},
+	setPath(path: string) {
+		//动态设置路由，不能使用location.href，否则会刷新页面
+		window.history.pushState({}, '', path);
+	},
+	setHash(hash: string) {
+		window.location.hash = hash;
+	},
+	setQuery(name: string, value: string) {
+		//首先获取当前path
+		let path = this.getPath();
+		//判断是否已经有查询参数
+		const query = this.getQuery(name);
+		if (checkNull(query)) {
+			//path是否有查询参数
+			if (path.indexOf('?') > -1) {
+				path += `&${name}=${value}`;
+			} else {
+				path += `?${name}=${value}`;
+			}
+		} else {
+			//找到原有的查询参数的全部内容
+			const reg = new RegExp(`(${name}=)([^&]*)`, 'i');
+			//替换原有的查询参数
+			path = path.replace(reg, `$1${value}`);
+		}
+		//设置新的path
+		this.setPath(path);
+	}
 };
